c266fa99ce4b13e0e9d55df715f7b553044669fb
nadipaca <nadipaca@mail.uc.edu>
2025-06-18 18:22:22 -0400

    Added delete, post and toggle feature using SB

--- todo/src/main/java/com/java/projects/todo/controller/TaskController.java
+++ todo/src/main/java/com/java/projects/todo/controller/TaskController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,6 +22,24 @@ public class TaskController {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks",tasks);
         return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
     }
 
 }
